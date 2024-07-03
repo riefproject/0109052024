81d4bf1ff1736b2cc431f2a906081241a1595a94
riefproject <riefedu@gmail.com>
2024-07-04 03:08:43 +0700

    countdown added

---
+++ countdown/main.js
@@ -0,0 +1,28 @@
+function countdownTimer() {
+    const endDate = new Date("Sep 1, 2024 00:00:40").getTime();
+    const now = new Date().getTime();
+    const timeLeft = endDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (timeLeft < 0) {
+        clearInterval(timer);
+        // Redirect to main content page
+        window.location.href = "../index.html"; // Adjust the path as needed
+    } else {
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+}
+
+const timer = setInterval(countdownTimer, 1000);
